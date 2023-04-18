0ef19c17b3ad5f2b11d043656f6934f2362a16dd
redffish <he279@wisc.edu>
2023-04-18 15:27:50 -0500

    tests for M1

---
+++ DB_M1/test3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# test3: tests range and select requests
+
+# range
+# keys in [1,4] first version
+./send.py range 1 4 0
+
+# keys in [10, 15] third version
+./send.py range 10 15 2
+
+# select
+# >
+./send.py select gt 1022 0
+
+# >=
+./send.py select gteq 1022 0
+
+# <
+./send.py select ls 12 0
+./send.py select ls 12 1
+
+# <=
+./send.py select lseq 12 0
+./send.py select lseq 12 1
+
+# ==
+./send.py select eq 512 1
+./send.py select eq 513 2
